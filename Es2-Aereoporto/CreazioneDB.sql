0c81abeeaf8cdf73b32ed61c7f7e4442ccac72d5
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-09-20 11:21:07 +0200

    Caricamento esercizio aeroporto

---
+++ Es2-Aereoporto/CreazioneDB.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE Aeroporto(
+Citta VARCHAR(30) PRIMARY KEY,
+Nazione VARCHAR(3),
+NumPiste INT)
+
+CREATE TABLE Volo(
+IdVolo INT PRIMARY KEY IDENTITY,
+GiornoSettimana VARCHAR(10),
+CittaPartenza VARCHAR(10),
+OraPartenza TIME,
+CittaArrivo VARCHAR(10),
+OraArrivo TIME,
+TipoAereo VARCHAR(15))
+
+
+CREATE TABLE Aereo(
+TipoAereo VARCHAR(10) PRIMARY KEY,
+NumPasseggeri INT,
+QtaMerci INT,
+)
---
+++ Es2-Aereoporto/Es2-Aereoporto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es2_Aereoporto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
