d7b7080707466eabdc5abad7aa876efaae1a3d00
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-09-24 10:43:35 +0200

    Esercizio aeroporto finito
    Completamento dell'esercizio dell'aeroporto con annesse query di ricerca sul database.

---
+++ Es2-Aereoporto/RicercaSuDatabase.sql
@@ -0,0 +1,22 @@
+﻿/*Le città da cui partono voli per Roma in ordine alfabetico*/
+
+SELECT DISTINCT v.CittaPartenza
+FROM Volo v
+WHERE v.CittaArrivo = 'Roma'
+ORDER BY v.CittaPartenza;
+
+/*città con aeroporto dove non è noto il numero di piste*/
+
+SELECT DISTINCT a.Citta
+FROM Aeroporto a
+WHERE NumPiste IS NULL;
+
+/*Per ogni volo misto(sia merci che passeggeri) codice volo e dati di trasporto*/
+
+SELECT v.IdVolo, a.QtaMerci, a.NumPasseggeri
+FROM Aereo a, Volo v
+WHERE a.TipoAereo = v.TipoAereo
+AND a.QtaMerci>0
+AND a.NumPasseggeri>0;
+
+/*
