4a66c30b369b4831418d66ea56f708d881c9dde0
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-09-30 12:58:48 +0200

    Es4-Creazione tabella

---
+++ Es4-Museo/Es4-Museo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es4_Museo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Es4-Museo/Query/CreazioneTabelle.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE Museo (
+ NomeM VARCHAR(20) PRIMARY KEY,
+ Citta VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE Artista (
+ NomeA VARCHAR(20) PRIMARY KEY,
+ Nazionalita VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE Opera (
+ Codice INT PRIMARY KEY IDENTITY,
+ Titolo VARCHAR(20) NOT NULL,
+ NomeM VARCHAR(20) NOT NULL,
+ NomeA VARCHAR(20) NOT NULL,
+ FOREIGN KEY (NomeM) REFERENCES Museo(NomeM),
+ FOREIGN KEY (NomeA) REFERENCES Artista(NomeA)
+)
+
+CREATE TABLE Personaggio (
+ Personaggio VARCHAR(20) PRIMARY KEY,
+ Codice INT,
+ FOREIGN KEY (Codice) REFERENCES Opera(Codice)
+)
+
+INSERT INTO Museo VALUES
+('Louvre','FR'),
+('Uffizzi','IT'),
+('Prado', 'ES');
+
+INSERT INTO Artista VALUES
+('Picasso','ES'),
+('Monet','FR'),
+('Giotto', 'IT');
+INSERT INTO Opera VALUES
+('Dipinto','Louvre','Picasso'),
+('Scultura','Uffizzi','Monet'),
+('Dipinto','Prado','Giotto');
+
+INSERT INTO Personaggio VALUES 
+('David',1),
+('Gioconda',2),
+('Orologi sciolti',2);
