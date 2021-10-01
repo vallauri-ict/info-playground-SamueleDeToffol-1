a796d67e74c36d769e0801c93f5401f8ca104bb5
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-10-01 11:29:34 +0200

    Aggiunta di Query per ricerche

---
+++ Es4-Museo/Query/RicercheSuDb.sql
@@ -0,0 +1,14 @@
+﻿/* Per ciascun museo di Londra il numero di opere di artisti italiani ivi conservati*/
+SELECT m.NomeM, count(*) as NumeroOpere
+FROM Artista a, Museo m, Opera o
+WHERE a.Nazionalita = 'IT'
+AND a.NomeA = o.NomeA 
+AND m.NomeM = o.NomeM
+AND m.Citta = 'Londra'
+GROUP BY m.NomeM
+
+/*Il nome di musei di Londra che non conservano opere di Tiziano*/
+SELECT m.NomeM
+FROM Museo m
+WHERE m.Citta = 'Londra'
+AND NOT EXISTS (SELECT * FROM Opera o WHERE o.NomeA = 'Tiziano' AND o.NomeM = m.NomeM)
