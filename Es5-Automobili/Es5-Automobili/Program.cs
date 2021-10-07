c68b4c315d4d59f945380576aae4719d6d04e91b
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-10-07 15:30:01 +0200

    Es5-Automobili : creazione tabelle e inserimento dati

---
+++ Es5-Automobili/Es5-Automobili/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es5_Automobili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
