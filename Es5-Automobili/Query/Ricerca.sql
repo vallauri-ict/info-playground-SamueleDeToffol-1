f023130de41c5457bf8500b3ae662157cb13b080
SamueleDeToffol <s.detoffol.0728@vallauri.edu>
2021-10-09 07:35:27 +0200

    Es5 - Bug fixing

--- Es5-Automobili/Query/Ricerca.sql
+++ Es5-Automobili/Query/Ricerca.sql
@@ -1,13 +1,72 @@
-﻿SELECT Targa,Marca FROM Automobile
-WHERE Potenza>120 OR Cilindrata>2000;
+﻿/*1- Targa e Marca delle Auto di cilindrata superiore a 2000 cc o di potenza superiore a 120 CV*/
+SELECT a.Targa, a.Marca
+FROM Automobili a
+WHERE a.Cilindrata > 2000
+OR a.Potenza > 120
 
-SELECT Nome,Marca FROM Automobile, Proprietario
-WHERE Proprietario.CodF=Automobile.CodF AND Potenza>120 OR Cilindrata>2000;
+/*2- Nome del proprietario e Targa delle Auto di cilindrata superiore a 2000 cc oppure di potenza superiore a 120 CV*/
+SELECT p.Nome, a.Targa
+FROM Automobili a, Proprietari p
+WHERE a.CodF = p.CodF
+AND (a.Cilindrata > 2000 OR a.Potenza > 120)
 
-SELECT p.Nome,Targa FROM Automobile, Proprietario p, Assicurazione a
-WHERE p.CodF=Automobile.CodF AND Automobile.CodAss=a.CodAss
-AND (Potenza>120 OR Cilindrata>2000) AND a.Nome='Sara';
+/*3- Targa e Nome del proprietario delle Auto di cilindrata superiore a 2000 cc oppure di potenza superiore a 120 CV, assicurate presso la “SARA”*/
+SELECT a.Targa, p.Nome
+FROM Automobili a, Proprietari p, Assicurazioni ass
+WHERE a.CodF = p.CodF
+AND a.CodAss = ass.CodAss
+AND (a.Cilindrata > 2000 OR a.Potenza > 120)
+AND ass.Nome = 'SARA'
 
-SELECT DISTINCT p.Nome,au.Targa FROM Automobile au, Proprietario p, Assicurazione a, Sinistro s, AutoCoinvolta
-WHERE p.CodF=au.CodF AND au.CodAss=a.CodAss AND s.CodS=AutoCoinvolta.CodS AND AutoCoinvolta.Targa=au.Targa
-AND s.Data='2002-01-20' AND a.Nome='Sara'
+/*4- Targa e Nome del proprietario delle Auto assicurate presso la “SARA” e coinvolte in sinistri il 20/01/02*/
+SELECT a.Targa, p.Nome
+FROM Automobili a, Proprietari p, Assicurazioni ass, Autocoinvolte ac, Sinistri s
+WHERE a.CodF = p.CodF
+AND a.CodAss = ass.CodAss
+AND ass.Nome = 'SARA'
+AND ac.CodS = s.CodS
+AND s.Giorno = '2002-01-20'
+
+/*5- Per ciascuna Assicurazione, il nome, la sede ed il numero di auto assicurate*/
+SELECT ass.Nome, ass.Sede, COUNT(*) AS NumeroAutoAssicurate
+FROM Assicurazioni ass, Automobili a
+WHERE ass.CodAss = a.CodAss
+GROUP BY ass.Nome, ass.Sede, a.CodAss
+
+/*6- Per ciascuna auto “Fiat”, la targa dell’auto ed il numero di sinistri in cui è stata coinvolta*/
+SELECT a.Targa, COUNT(*) AS NumeroSinistri
+FROM Automobili a, Autocoinvolte ac
+WHERE a.Marca = 'FIAT'
+AND a.Targa = ac.Targa
+GROUP BY a.Targa, ac.CodS
+
+/*7- Per ciascuna auto coinvolta in più di un sinistro, la targa dell’auto, il nome dell’ Assicurazione ed il totale dei danni riportati*/
+SELECT a.Targa, ass.Nome, SUM(ac.ImportoDelDanno) AS TotaleDanni
+FROM Automobili a, Assicurazioni ass, Autocoinvolte ac
+WHERE a.CodAss = ass.CodAss
+AND a.Targa = ac.Targa
+GROUP BY a.Targa, ass.Nome, ac.Targa
+HAVING COUNT(*) > 1
+
+/*8- CodF e Nome di coloro che possiedono più di un’auto*/
+SELECT p.CodF, p.Nome
+FROM Proprietari p, Automobili a
+WHERE p.CodF = a.CodF
+GROUP BY p.CodF, p.Nome
+HAVING COUNT(*) > 1
+
+/*9- La targa delle auto che non sono state coinvolte in sinistri dopo il 20/01/01*/
+SELECT a.Targa
+FROM Automobili a
+WHERE NOT EXISTS(SELECT * 
+				 FROM Autocoinvolte ac, Sinistri s
+				 WHERE a.Targa = ac.Targa
+				 AND ac.CodS = s.CodS
+				 AND s.Giorno > '2001-01-20')
+
+/*10- Il codice dei sinistri in cui non sono state coinvolte auto con cilindrata inferiore a 2000 cc*/
+SELECT s.CodS
+FROM Automobili a, Autocoinvolte ac, Sinistri s
+WHERE a.Targa = ac.Targa
+AND ac.CodS = s.CodS
+AND a.Cilindrata > 2000
